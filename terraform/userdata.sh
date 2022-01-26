aad730388f29f6e9e8cf1f6887f5deb0ff8fd750
Vigneshvj2503 <77635231+Vigneshvj2503@users.noreply.github.com>
2022-01-26 15:47:54 +0530

    Delete userdata.sh

--- terraform/userdata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-aws eks update-kubeconfig --name eks_cluster_atc
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl version --client
