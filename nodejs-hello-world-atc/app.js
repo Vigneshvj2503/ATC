43250b6edcef42438f0b372bc4c471549f2e0cc4
Vigneshvj2503 <77635231+Vigneshvj2503@users.noreply.github.com>
2022-01-26 13:39:04 +0530

    Update app.js

--- nodejs-hello-world-atc/app.js
+++ nodejs-hello-world-atc/app.js
@@ -1,5 +1,16 @@
-const express = require('express')
-const app = express()
+'use strict';
 
-app.get('/', (req, res) => res.send('Hello ATC! Devops Task Completed'))
-app.listen(8080, () => console.log('Server ready'))
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello ATC! DevOps Task Completed');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
